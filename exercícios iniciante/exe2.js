1c2dd2d8677d9d380e7815449a9d2c9e8454655d
Lorrana dos Santos Bomfim <lorranasantosbomfim@hotmail.com>
2026-08-23 11:21:11 -0300

    Add login verification logic in exe2.js
    Implement login verification for employees with correct username and password.

---
+++ exercícios iniciante/exe2.js
@@ -0,0 +1,20 @@
+/* # Exercício 2 — Verificação de login
+
+Uma empresa quer validar login de funcionários.
+
+Objetivo
+Crie:
+- usuário correto
+- senha correta
+Depois:
+- compare com valores digitados
+- mostre:
+    - “Login realizado”
+    - ou “Usuário ou senha inválidos” */
+var usuário = "Rafael"
+var senha = 302010
+if ((usuário == "Lorrana") && (senha == "280472")){
+    console.log ("Login realizado!")
+} else {
+    console.log ("Usuário e senha incorretos,tente novamente!")
+}
